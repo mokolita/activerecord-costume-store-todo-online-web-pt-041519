8d2637a1b41a22d4af8ac297ffd36def3f27faad
Meg Galvez <meg@oakmaine.com>
2019-07-08 20:11:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_costumes.rb
+++ db/migrate/01_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change 
-    create_table :costumes do |c|
+    create_table :costumes do |t|
       t.string :name 
       t.integer :price 
       t.string :size 
